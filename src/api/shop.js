91c042720b2f763868a76b41544650e047fce4e6
Mario Quintana <pyrros2000@gmail.com>
2019-02-28 11:23:03 -0800

    Consol logging each item by mapping and not just the array

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,25 +1,28 @@
 
 // import _products from './products.json';
 
-// const TIMEOUT = 100
-
 // export default {
-//   getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
-//   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
+//   getProducts: cb => cb(_products),    
+//   buyProducts: (payload,cb) => cb()
 // }
 
 const URL = 'http://tech.work.co/shopping-cart/products.json'
 
 const fetchData = () => {
-  return fetch(URL)
+  fetch(URL)
     .then(response => response.json())
-    .then(response => {
-      console.log(response[0], 'Products') 
-  })
+    .then(items => {
+      items.map(product => 
+      console.log(product))
+    })  
 }
 
 export default {
   getProducts: cb => fetchData(URL), 
-  buyProducts: (payload,cb) => cb()
+  buyProducts: (payload, cb) => cb()
 }
 
+// export default {
+//   getProducts: (cb, timeout) => setTimeout(() => cb(fetchData().then(response => response)), timeout || TIMEOUT),
+//   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT),
+// }
